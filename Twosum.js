5e31ee7687bce594385b567d2afc26df4e9c6c38
TimurProg <timurnurgayanov@gmail.com>
2026-03-15 23:23:59 +0500

    add problem Twosum.js

---
+++ Twosum.js
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/two-sum/
+const twoSum = (nums,target) => {
+    for(let i = 0 ; i < nums.length; i++){
+        let mas = [];
+        mas.push(i);
+        sums = nums[i];
+        for (let j = i+1 ; j < nums.length; j++){
+            if (sums + nums[j] === target){
+                mas.push(j);
+                return mas;
+            }
+        }
+    }
+}
